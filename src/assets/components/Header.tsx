cc54099399f088a63c9cb27bcf1734709dcfb668
Moti7950 <shalom7950@gmail.com>
2025-08-31 08:54:25 +0300

    finish card

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,21 +1,37 @@
 export default function Header(props: {
   statusNow: boolean;
-  setstatus: Function;
+  setStatus: Function;
+  modeCard: boolean;
+  setList: Function;
 }) {
   return (
     <>
+      <h1>Campus Club Dashboard</h1>
       <section id="Header-controler">
-        <h1>Campus Club Dashboard</h1>
         <input
           id="header-checkbox"
           type="checkbox"
           onClick={() => {
-            props.setstatus((prev: boolean) => !prev);
+            props.setStatus((prev: boolean) => !prev);
           }}
         />
         <label htmlFor="header-checkbox">Show only active members</label>
-        <button id="header-List">List</button>
-        <button id="header-Grid">Grid</button>
+        <button
+          id="header-List"
+          onClick={() => {
+            props.setList(false);
+          }}
+        >
+          List
+        </button>
+        <button
+          id="header-Grid"
+          onClick={() => {
+            props.setList(true);
+          }}
+        >
+          Grid
+        </button>
         <input type="text" placeholder="Serch members by name" />
       </section>
     </>
